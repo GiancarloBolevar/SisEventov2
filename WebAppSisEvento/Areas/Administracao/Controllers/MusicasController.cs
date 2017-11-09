251e1a2bb0acb9ea4212c597b714b918edb47c2e
giancarlo souza <giancarlo_lorraine@hotmail.com>
2017-11-09 20:53:27 -0300

    Adiciona CRUD de Musica e configura o Autorize nos controllers

--- WebAppSisEvento/Areas/Administracao/Controllers/MusicasController.cs
+++ WebAppSisEvento/Areas/Administracao/Controllers/MusicasController.cs
@@ -8,110 +8,115 @@ using System.Web;
 using System.Web.Mvc;
 using WebAppSisEvento.Models;
 
-namespace Areas.Administracao.Controllers
+namespace WebAppSisEvento.Areas.Administracao.Controllers
 {
     [Authorize(Roles = "Administrador")]
-    public class GenerosController : Controller
+    public class MusicasController : Controller
     {
         private WebAppSisEventoContext db = new WebAppSisEventoContext();
 
-        // GET: Administracao/Generos
+        // GET: Administracao/Musicas
         public ActionResult Index()
         {
-            return View(db.Generos.ToList());
+            var musicas = db.Musicas.Include(m => m.Genero);
+            return View(musicas.ToList());
         }
 
-        // GET: Administracao/Generos/Details/5
+        // GET: Administracao/Musicas/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Genero genero = db.Generos.Find(id);
-            if (genero == null)
+            Musica musica = db.Musicas.Find(id);
+            if (musica == null)
             {
                 return HttpNotFound();
             }
-            return View(genero);
+            return View(musica);
         }
 
-        // GET: Administracao/Generos/Create
+        // GET: Administracao/Musicas/Create
         public ActionResult Create()
         {
+            ViewBag.GeneroId = new SelectList(db.Generos, "GeneroId", "Descricao");
             return View();
         }
 
-        // POST: Administracao/Generos/Create
+        // POST: Administracao/Musicas/Create
         // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
         // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "GeneroId,Descricao")] Genero genero)
+        public ActionResult Create([Bind(Include = "MusicaId,Descricao,GeneroId")] Musica musica)
         {
             if (ModelState.IsValid)
             {
-                db.Generos.Add(genero);
+                db.Musicas.Add(musica);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(genero);
+            ViewBag.GeneroId = new SelectList(db.Generos, "GeneroId", "Descricao", musica.GeneroId);
+            return View(musica);
         }
 
-        // GET: Administracao/Generos/Edit/5
+        // GET: Administracao/Musicas/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Genero genero = db.Generos.Find(id);
-            if (genero == null)
+            Musica musica = db.Musicas.Find(id);
+            if (musica == null)
             {
                 return HttpNotFound();
             }
-            return View(genero);
+            ViewBag.GeneroId = new SelectList(db.Generos, "GeneroId", "Descricao", musica.GeneroId);
+            return View(musica);
         }
 
-        // POST: Administracao/Generos/Edit/5
+        // POST: Administracao/Musicas/Edit/5
         // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
         // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "GeneroId,Descricao")] Genero genero)
+        public ActionResult Edit([Bind(Include = "MusicaId,Descricao,GeneroId")] Musica musica)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(genero).State = EntityState.Modified;
+                db.Entry(musica).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(genero);
+            ViewBag.GeneroId = new SelectList(db.Generos, "GeneroId", "Descricao", musica.GeneroId);
+            return View(musica);
         }
 
-        // GET: Administracao/Generos/Delete/5
+        // GET: Administracao/Musicas/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Genero genero = db.Generos.Find(id);
-            if (genero == null)
+            Musica musica = db.Musicas.Find(id);
+            if (musica == null)
             {
                 return HttpNotFound();
             }
-            return View(genero);
+            return View(musica);
         }
 
-        // POST: Administracao/Generos/Delete/5
+        // POST: Administracao/Musicas/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            Genero genero = db.Generos.Find(id);
-            db.Generos.Remove(genero);
+            Musica musica = db.Musicas.Find(id);
+            db.Musicas.Remove(musica);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
